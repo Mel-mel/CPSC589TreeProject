f085901b5fdbe1f3b3f97f166ab755f0b64e72e5
Melissa <la_la_la_la_1@hotmail.com>
2016-03-17 15:30:29 -0600

    Code that draws a leaf-less tree
    Have cylinders working and rendering properly

--- Tree.cpp
+++ Tree.cpp
@@ -10,10 +10,12 @@
 #include "Tree.h"
 using namespace std; 
 using namespace glm;
-int num = 10; // this will be determined by leaf span given by user
+int num = 4; // this will be determined by leaf span given by user
 int stopPoint = num+1;
 int index = 1;
 float length = 0.5f; //The initial length of the trunk/branch
+float theta = M_PI/6;
+float temp = 0.5f;
 
 //Values to change how the branches are drawn
 float x_change = 0.1f;
@@ -21,7 +23,18 @@ float y_change = 0.1f;
 
 vector<glm::vec2> splitPoints; // keeps track of point where one branch becomes two 
 
+GLUquadricObj *quadratic;
 
+/*void Tree::initializeCylinders()
+{
+	quadratic = gluNewQuadric();
+	gluQuadricNormals(quadratic, GLU_SMOOTH);
+}*/
+
+Tree::Tree()
+{
+	//Blank initializer
+}
 
 //This is the constructor for a tree (need to find the radius later)
 Tree::Tree(vec2 hBase, vec2 hTop, vec2 w1, vec2 w2)
@@ -31,7 +44,7 @@ Tree::Tree(vec2 hBase, vec2 hTop, vec2 w1, vec2 w2)
 	width1 = w1;
 	width2 = w2;
 
-	float temp = 0.5f;
+	
 	//vec2 startpoint (heightBase.x, heightBase.y);
 	vec2 startpoint(0.0f, -0.9);
 	//glBegin(GL_POINTS);
@@ -54,6 +67,84 @@ Tree::Tree(vec2 hBase, vec2 hTop, vec2 w1, vec2 w2)
 	x_change = 0.5f;
 	y_change = 0.5f;
 	
+}
+
+//The following two functions below were used to create a custom cylinder based on this website
+//http://lifeofaprogrammergeek.blogspot.ca/2008/07/rendering-cylinder-between-two-points.html
+//Accessed on Mar 17, 2016 (do the proper citing later~)
+//renderCylinder and renderCylinder_convenient
+void Tree::renderCylinder(float x1, float y1, float z1, float x2,float y2, float z2, float radius,int subdivisions,GLUquadricObj *quadric)
+{
+	float vx = x2-x1;
+	float vy = y2-y1;
+	float vz = z2-z1;
+
+	//handle the degenerate case of z1 == z2 with an approximation
+	if(vz == 0)
+		vz = .0001;
+
+	float v = sqrt( vx*vx + vy*vy + vz*vz );
+	float ax = 57.2957795*acos( vz/v );
+	if ( vz < 0.0 )
+		ax = -ax;
+	float rx = -vy*vz;
+	float ry = vx*vz;
+	glPushMatrix();
+
+	//draw the cylinder body
+	glTranslatef( x1,y1,z1 );
+	glRotatef(ax, rx, ry, 0.0);
+	gluQuadricOrientation(quadric,GLU_OUTSIDE);
+	gluCylinder(quadric, radius, radius, v, subdivisions, 1);
+
+	//draw the first cap
+	gluQuadricOrientation(quadric,GLU_INSIDE);
+	gluDisk( quadric, 0.0, radius, subdivisions, 1);
+	glTranslatef( 0,0,v );
+
+	//draw the second cap
+	gluQuadricOrientation(quadric,GLU_OUTSIDE);
+	gluDisk( quadric, 0.0, radius, subdivisions, 1);
+	glPopMatrix();
+}
+void Tree::renderCylinder_convenient(float x1, float y1, float z1, float x2,float y2, float z2, float radius,int subdivisions)
+{
+	//the same quadric can be re-used for drawing many cylinders
+	GLUquadricObj *quadric=gluNewQuadric();
+	gluQuadricNormals(quadric, GLU_SMOOTH);
+	renderCylinder(x1,y1,z1,x2,y2,z2,radius,subdivisions,quadric);
+	gluDeleteQuadric(quadric);
+}
+
+void Tree::renderTreeStageTwo()
+{
+	/*glTranslatef(0.0f,-0.7f,0.0f); //-0.7 should be replaced with first point given as height
+	glRotatef(180.0f, 0.0f, 1.0f, 1.0f);
+	glBegin(GL_POLYGON);
+	
+	
+	glColor3f(1.0f, 0.8f, 0.2f);
+	
+	gluCylinder(quadratic,0.015f,0.015f,0.9f,50,50);
+	
+	glEnd();*/
+	//vec2 startpoint (heightBase.x, heightBase.y);//This is for the user input
+	vec2 startpoint(0.0f, -0.9);
+	splitPoints.push_back(vec2(startpoint.x, startpoint.y));
+	splitPoints.push_back(vec2(startpoint.x, startpoint.y + temp));
+	renderCylinder_convenient(splitPoints[0].x, splitPoints[0].y, 0, splitPoints[1].x, splitPoints[1].y, 0, 0.025, 10);
+	fractals(index);
+	for(int i = 1; i < splitPoints.size()-stopPoint; i++)
+	{
+		glColor3f(0.0f, 1.0f, 0.0f);
+		renderCylinder_convenient(splitPoints[i].x, splitPoints[i].y, 0, splitPoints[2*i].x, splitPoints[2*i].y, 0, 0.025, 10); 
+	} 
+	for(int i = 1; i < splitPoints.size()-stopPoint; i++)
+	{
+		glColor3f(1.0f, 0.8f, 0.2f);
+		renderCylinder_convenient(splitPoints[i].x, splitPoints[i].y, 0, splitPoints[2*i+1].x, splitPoints[2*i+1].y, 0, 0.025, 10); 
+	} 
+	splitPoints.clear();
 }
 
 void Tree::renderTreeStageOne()
@@ -118,15 +209,15 @@ void Tree::fractals(int n)
 	{
 		//Left branch
 		vec2 leftTemp = splitPoints[n];
-		leftTemp.x = (leftTemp.x - x_change)*cos(M_PI/6);
+		leftTemp.x = (leftTemp.x - x_change)*sin(theta);
 		//leftTemp.y = leftTemp.y + y_change;
-		leftTemp.y = (leftTemp.y + y_change)*sin(M_PI/6);
+		leftTemp.y = (leftTemp.y + y_change)*cos(-theta);
 		splitPoints.push_back(leftTemp);
 		
 		//Right branch
 		vec2 rightTemp = splitPoints[n];
-		rightTemp.x = (rightTemp.x + x_change)*cos(-M_PI/6);
-		rightTemp.y = (rightTemp.y +  y_change)*sin(M_PI/6);
+		rightTemp.x = (rightTemp.x + x_change)*sin(theta);
+		rightTemp.y = (rightTemp.y +  y_change)*cos(theta);
 		splitPoints.push_back(rightTemp);
 	}
 	//Generate the left and right branches
@@ -140,17 +231,24 @@ void Tree::fractals(int n)
 		{
 			x_change = 0.01;
 		}
+		
+		
+		//Calculate cos and sin as individual variables
+		//Then take that values and times it by 2?
+		//Then add it to the change values
+		
+		
 		//Left branch
 		vec2 leftTemp = splitPoints[n];
-		leftTemp.x = (leftTemp.x - x_change)*cos(M_PI/6);
-		leftTemp.y = (leftTemp.y + y_change)*sin(M_PI/6);
+		leftTemp.x = (leftTemp.x - x_change)*sin(theta);
+		leftTemp.y = (leftTemp.y + y_change)*cos(-theta);
 		splitPoints.push_back(leftTemp);
 		
 		//Right branch
 		vec2 rightTemp = splitPoints[n];
-		rightTemp.x = (rightTemp.x + x_change)*cos(-M_PI/6);
+		rightTemp.x = (rightTemp.x + x_change)*sin(theta);
 		//rightTemp.y = rightTemp.y +  y_change;
-		rightTemp.y = (rightTemp.y +  y_change)*sin(M_PI/6);
+		rightTemp.y = (rightTemp.y +  y_change)*cos(theta);
 		splitPoints.push_back(rightTemp);
 		/*if(x_change > 0.02)
 		{
@@ -178,7 +276,4 @@ void Tree::fractals(int n)
 	}
 	*/
 }
-
-
-
 
