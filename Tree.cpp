98555767886c183d7b5e85f2eb94070dc0299f8b
crmikala <claire.mikalauskas@gmail.com>
2016-03-17 11:07:48 -0600

    Fractal tree working/trying to rotate the branches
    Dumb mistake found and added cos/sin rotations when saving points with fractals.

--- Tree.cpp
+++ Tree.cpp
@@ -10,14 +10,14 @@
 #include "Tree.h"
 using namespace std; 
 using namespace glm;
-int num = 4; // this will be determined by leaf span given by user
+int num = 10; // this will be determined by leaf span given by user
 int stopPoint = num+1;
 int index = 1;
 float length = 0.5f; //The initial length of the trunk/branch
 
 //Values to change how the branches are drawn
-float x_change = 0.5f;
-float y_change = 0.5f;
+float x_change = 0.1f;
+float y_change = 0.1f;
 
 vector<glm::vec2> splitPoints; // keeps track of point where one branch becomes two 
 
@@ -27,15 +27,10 @@ vector<glm::vec2> splitPoints; // keeps track of point where one branch becomes 
 Tree::Tree(vec2 hBase, vec2 hTop, vec2 w1, vec2 w2)
 {
 	heightBase = hBase;
-	cout << hBase.x << "  " << hBase.y << endl;
 	heightTop = hTop;
 	width1 = w1;
 	width2 = w2;
-	
-}
 
-void Tree::renderTreeStageOne()
-{
 	float temp = 0.5f;
 	//vec2 startpoint (heightBase.x, heightBase.y);
 	vec2 startpoint(0.0f, -0.9);
@@ -53,9 +48,16 @@ void Tree::renderTreeStageOne()
 	
 	splitPoints.push_back(vec2(startpoint.x, startpoint.y));
 	splitPoints.push_back(vec2(startpoint.x, startpoint.y + temp));
+	glEnd();
 	
 	fractals(index);
+	x_change = 0.5f;
+	y_change = 0.5f;
 	
+}
+
+void Tree::renderTreeStageOne()
+{	
 	for(int j = 0; j < splitPoints.size()- stopPoint; j++)
 	{
 		cout << "INDEX  " << j << endl;
@@ -67,103 +69,98 @@ void Tree::renderTreeStageOne()
 		cout << "x  " << splitPoints[j].x << "  " << "y  " << splitPoints[j].y << endl;
 		cout << "x  " << splitPoints[2*j+1].x << "  " << "y  " << splitPoints[2*j+1].y << endl;
 	}
+	glBegin(GL_LINES);
 	
 	for(int i = 1; i < splitPoints.size()- stopPoint; i++)
 	{
+		/*if(i % 2 == 1)
+		{
+			glTranslatef(-splitPoints[i].x, -splitPoints[i].y, 0.0f);
+			glRotatef(50, 1.0f, 0.0f,0.0f);
+			glTranslatef(splitPoints[i].x, splitPoints[i].y, 0.0f);
+			glBegin(GL_LINES);
+		}
+		else
+		{
+			glBegin(GL_LINES);
+		}*/
 		
+		
+		
+		glColor3f(1.0f, 0.0f, 0.0f);
 		//First line
 		glVertex2f(splitPoints[i].x, splitPoints[i].y);
-		glVertex2f(splitPoints[2*i].x, splitPoints[2*1].y);
+		glVertex2f(splitPoints[2*i].x, splitPoints[2*i].y);
+
 		
-		//Second line 
-		//glVertex2f(splitPoints[i].x, splitPoints[i].y);
-		//glVertex2f(splitPoints[2*i+1].x, splitPoints[2*i+1].y);
 	}
 	
-	glEnd();
-	//glBegin(GL_POINTS);
-	glBegin(GL_LINES);
-	glColor3f(0.0f, 1.0f, 0.0f);
+	
 	for(int i = 1; i < splitPoints.size()- stopPoint; i++)
 	{
-		
 		//Second line 
+//glBegin(GL_LINES);
+glColor3f(0.0f, 1.0f, 0.0f);
 		glVertex2f(splitPoints[i].x, splitPoints[i].y);
 		glVertex2f(splitPoints[2*i+1].x, splitPoints[2*i+1].y);
+//glEnd();
 	}
-	splitPoints.clear();
-	
-	
-	/*int i = 1;
-	splitPoints.push_back({splitPoints[i].x - x_change, splitPoints[i].y + y_change});//Left branch
-	splitPoints.push_back({splitPoints[i].x + x_change, splitPoints[i].y + y_change});//Right branch
-	glVertex2f(splitPoints[i].x, splitPoints[i].y);
-	glVertex2f(splitPoints[2*i].x, splitPoints[2*i].y);
-	glVertex2f(splitPoints[i].x, splitPoints[i].y);
-	glVertex2f(splitPoints[2*i+1].x, splitPoints[2*i+1].y);
-	
-	for(i = 1; i < num; i++)
-	{
-		
-		splitPoints.push_back({splitPoints[i].x - x_change, splitPoints[i].y + y_change});//Left branch
-		splitPoints.push_back({splitPoints[i].x + x_change, splitPoints[i].y + y_change});//Right branch
-		glVertex2f(splitPoints[i].x, splitPoints[i].y);
-		glVertex2f(splitPoints[2*i].x, splitPoints[2*i].y);
-		glVertex2f(splitPoints[i].x, splitPoints[i].y);
-		glVertex2f(splitPoints[2*i+1].x, splitPoints[2*i+1].y);
-		
-		/*glVertex2f(splitPoints[i].x, splitPoints[i].y);
-		glVertex2f(splitPoints[i+1].x, splitPoints[i+1].y);
-		glVertex2f(splitPoints[i].x, splitPoints[i].y);
-		glVertex2f(splitPoints[i+2].x, splitPoints[i+2].y);
-		x_change -= 0.05f;
-		y_change += 0.05f;*/
-		
-	/*}
-	
-	for(int j = 0; j < splitPoints.size(); j++)
-	{
-		cout << "j  " << j << endl;
-		cout << "x   " << splitPoints[j].x << "  y   ";
-		cout << splitPoints[j].y << endl;
-		
-	}
-	splitPoints.clear();*/
 	glEnd();
+	splitPoints.clear();
 }
 
 //This will run with the trunk already placed inside the splitPoints vector array
 void Tree::fractals(int n)
 {	
+	
 	//The if should exit the function once the last set of branches are drawn...?
 	if(n == num)
 	{
 		//Left branch
 		vec2 leftTemp = splitPoints[n];
-		leftTemp.x = leftTemp.x - x_change;
-		leftTemp.y = leftTemp.y + y_change;
+		leftTemp.x = (leftTemp.x - x_change)*cos(M_PI/6);
+		//leftTemp.y = leftTemp.y + y_change;
+		leftTemp.y = (leftTemp.y + y_change)*sin(M_PI/6);
 		splitPoints.push_back(leftTemp);
 		
 		//Right branch
 		vec2 rightTemp = splitPoints[n];
-		rightTemp.x = rightTemp.x + x_change;
-		rightTemp.y = rightTemp.y + y_change;
+		rightTemp.x = (rightTemp.x + x_change)*cos(-M_PI/6);
+		rightTemp.y = (rightTemp.y +  y_change)*sin(M_PI/6);
 		splitPoints.push_back(rightTemp);
 	}
 	//Generate the left and right branches
 	else
 	{	
+		if(y_change < 0.01)
+		{
+			y_change = 0.01;
+		}
+		if(x_change < 0.01)
+		{
+			x_change = 0.01;
+		}
 		//Left branch
 		vec2 leftTemp = splitPoints[n];
-		leftTemp.x = leftTemp.x - x_change;
-		leftTemp.y = leftTemp.y + y_change;
+		leftTemp.x = (leftTemp.x - x_change)*cos(M_PI/6);
+		leftTemp.y = (leftTemp.y + y_change)*sin(M_PI/6);
 		splitPoints.push_back(leftTemp);
 		
 		//Right branch
 		vec2 rightTemp = splitPoints[n];
-		rightTemp.x = rightTemp.x + x_change;
-		rightTemp.y = rightTemp.y + y_change;
+		rightTemp.x = (rightTemp.x + x_change)*cos(-M_PI/6);
+		//rightTemp.y = rightTemp.y +  y_change;
+		rightTemp.y = (rightTemp.y +  y_change)*sin(M_PI/6);
 		splitPoints.push_back(rightTemp);
+		/*if(x_change > 0.02)
+		{
+			x_change -= 0.01;
+		}
+		if(y_change > 0.02)
+		{
+			y_change -= 0.01;
+		}*/
+		
 		fractals(n+1);
 		
 	}
